a6a24be8ed526ef8d357d11ddee007a81add1545
mirmik <mirmikns@yandex.ru>
2020-03-22 02:41:35 +0300

    'a'

--- atom/scanner.py
+++ atom/scanner.py
@@ -4,6 +4,8 @@ import atom.utils
 lastscan_time = 0
 lastscan_ips = []
 
+outed = dict()
+
 def serve(milliseconds):
 	global lastscan_time
 	global lastscan_ips
@@ -14,12 +16,19 @@ def serve(milliseconds):
 		ips = atom.utils.scan_network_doit()
 
 		for p in ips:
-			if p not in lastscan_ips:
+			if p not in lastscan_ips and not in outed:
 				atom.send_notify(f"Сканер зарегистрировал новую пару {p}")
+				if p[0] in outed:
+					del outed[p[0]]
 
 		for p in lastscan_ips:
 			if p not in ips:
-				atom.send_notify(f"Сканер зафиксировал исчезновение пары {p}")
+				outed[p[0]] = (milliseconds, p)
+				
+		for k, v in outed.items():
+			if milliseconds - v[0] > 100000:
+				atom.send_notify(f"Сканер зафиксировал исчезновение пары {v[1]}")
+				del outed[k]
 
 		lastscan_ips = ips
 
