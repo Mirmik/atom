80ad1e4c5531733c55c86eedc026ac09b551f818
mirmik <mirmikns@yandex.ru>
2020-03-22 01:25:11 +0300

    'a'

--- atom/fortunator.py
+++ atom/fortunator.py
@@ -132,7 +132,7 @@ class Fortunator(atom.dialog.Dialog):
 		if verb.normal_form in verbs_scan:
 			ret = self.scan_action(sents)
 
-		if verb.normal_form in reboot_action:
+		if verb.normal_form in verbs_reboot:
 			ret = self.reboot_action(sents)
 
 		if ret is not None and ret[1] > 0.00001:
